564b7ca4f5447504d960d310748c05106a3d3c11
Marwan <marwaneg@aucegypt.edu>
2024-05-18 20:00:22 +0300

    Fix the pagination buttons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -31,14 +31,8 @@ import { Router } from '@angular/router';
       </app-user>
     </section>
     <nav class="pagination">
-      <button>
-      <svg fill="#fff" height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path id="XMLID_222_" d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001 c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213 C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606 C255,161.018,253.42,157.202,250.606,154.389z"></path> </g></svg>
-      </button>
       <button (click)="navigateToHome(1)">1</button>
       <button (click)="navigateToHome(2)">2</button>
-      <button>
-        <svg fill="#fff" height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path id="XMLID_222_" d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001 c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213 C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606 C255,161.018,253.42,157.202,250.606,154.389z"></path> </g></svg>
-      </button>
     </nav>
   `,
   styleUrl: './home.component.css'
@@ -49,7 +43,7 @@ export class HomeComponent {
   filteredUserList: User[] = [];
   route: ActivatedRoute = inject(ActivatedRoute);
   constructor(private router: Router) {
-    const page = parseInt(this.route.snapshot.params['page']);
+    var page = parseInt(this.route.snapshot.params['page']);
     this.userService.getAllUsers(page).then((userList: User[]) => {
       this.userList = userList;
       this.filteredUserList = userList;
